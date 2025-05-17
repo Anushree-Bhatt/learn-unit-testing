97da8bc954d874b034a1dfce7713fe194a259901
Anushree <anushreebhatt357@gmail.com>
2025-05-16 23:38:00 -0400

    More Assertion methods

--- src/test/java/com/practice/junit/MyMathTest.java
+++ src/test/java/com/practice/junit/MyMathTest.java
@@ -3,8 +3,10 @@ package com.practice.junit;
 import com.practice.junit.calci.MyMath;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyMathTest {
 
@@ -18,5 +20,27 @@ public class MyMathTest {
     public void addTest_ZeroLengthArray(){
         MyMath math = new MyMath();
         assertEquals(0, math.add(new int[]{})); //sum = 0
+    }
+
+    @Test
+    public void testAsserts(){
+        List<String> courses = Arrays.asList("AWS", "Azure", "Devops");
+        String str1 = "Hey!";
+        String str2 = null;
+
+        Boolean test1 = courses.contains("AWS");
+        Boolean test2 = courses.contains("Docker");
+
+        assertTrue(test1);
+        assertFalse(test2);
+
+        assertNull(str2);
+        assertNotNull(str1);
+
+        assertEquals(10, 10);
+        assertEquals(3, courses.size());
+
+        assertArrayEquals(new int[]{1,2,3}, new int[]{1,2,3});
+
     }
 }
